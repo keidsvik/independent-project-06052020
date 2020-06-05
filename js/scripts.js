714175669d5093cea4ddf625b990191d65afc3d1
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-06-05 09:57:36 -0700

    add submit button to html form

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
   $('form#quiz').submit(function(event) {
-    event.preventDefault();
     const color = $("input:radio[name=color]:checked").val();
     const animal = $("input:radio[name=animal]:checked").val();
-    const whereyoulive = $("#whereyoulive").val();
-    const beverage = $("#beverage").val();
-    const meal = $("#meal").val();
+    const whereyoulive = $("select#whereyoulive").val();
+    const beverage = $("select#beverage").val();
+    const meal = $("select#meal").val();
+
+    if (color === 'red' && animal === 'eels' && whereyoulive === 'portland' && beverage === 'blood'&& meal === 'hot dog gelatin') {
+    $("#swift").show();
+    } else if (color === 'green' && animal === 'miniaturedonkey' && whereyoulive === 'quiveringwomb' && beverage === 'hoticecream' && meal === 'gravy') {
+    $("#go").show();
+    } else if (color === 'blue' && animal === 'pig' && whereyoulive === 'dancingqueen' && beverage === 'milk and whiskey on the rocks' && meal === 'milk meal') {
+    $("#python").show();
+    } else {
+    $("#nothing").show();
+    }
+    event.preventDefault();
   });
 });
