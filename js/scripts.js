ad014a3fb8040e620ded1b64ae48ab20aa1419ee
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-06-05 10:31:18 -0700

    add branching to js document so users may choose their answers and have it spit out results

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
   $('form#quiz').submit(function(event) {
+
     const color = $("input:radio[name=color]:checked").val();
     const animal = $("input:radio[name=animal]:checked").val();
     const whereyoulive = $("select#whereyoulive").val();
     const beverage = $("select#beverage").val();
     const meal = $("select#meal").val();
 
-    if (color === 'red' && animal === 'eels' && whereyoulive === 'portland' && beverage === 'blood'&& meal === 'hot dog gelatin') {
+    if (color === 'red' && animal === 'eels' && whereyoulive === 'portland' && beverage === 'blood'&& meal === 'hotdoggelatin') {
     $("#swift").show();
     } else if (color === 'green' && animal === 'miniaturedonkey' && whereyoulive === 'quiveringwomb' && beverage === 'hoticecream' && meal === 'gravy') {
-    $("#go").show();
-    } else if (color === 'blue' && animal === 'pig' && whereyoulive === 'dancingqueen' && beverage === 'milk and whiskey on the rocks' && meal === 'milk meal') {
     $("#python").show();
+    } else if (color === 'blue' && animal === 'pig' && whereyoulive === 'dancingqueen' && beverage === 'milknwhiskey' && meal === 'milkmeal') {
+    $("#go").show();
     } else {
     $("#nothing").show();
     }
