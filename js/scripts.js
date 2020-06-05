dae9bb54cc4b260b4f99ce5a386b5900b47289fe
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-06-05 09:27:57 -0700

    add variables so the values to be read in our form via js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
   $('form#quiz').submit(function(event) {
     event.preventDefault();
-
-});
+    const color = $("input:radio[name=color]:checked").val();
+    const animal = $("input:radio[name=animal]:checked").val();
+    const whereyoulive = $("#whereyoulive").val();
+    const beverage = $("#beverage").val();
+    const meal = $("#meal").val();
+  });
 });
